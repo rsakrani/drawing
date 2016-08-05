697795a5a66f9da200a62a6a9d24a8015d9b5964
Rehmat Sakrani <rsakrani@gmail.com>
2016-08-05 15:08:15 -0400

    eraser

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var myRed = 10;
+var myGreen = 0;
+var myBlue = 255;
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  noStroke();
+  fill(myRed, myGreen, myBlue);
+   if (mouseIsPressed) {
+    fill(255, 255, 255);
+  }
+  ellipse(mouseX, mouseY, 100, 100);
+  myRed = myRed + 0.5;
+  if (myRed > 255); {
+    myRed = 0;
+  }
+  myBlue = myBlue - 1;
+  if (myBlue <= 0) {
+    myBlue=255;
+  }
+  
+  
+}
